5f644711a9992687510c9d696d7ff7a8807457fa
SadCheeto <caspar.marschall@me.com>
2023-04-19 13:56:24 +0200

    added testing

---
+++ test_data_nested_controlflows/data_nested_controlflows_3.py
@@ -0,0 +1,72 @@
+i = 2
+
+if i == 2:
+    if i == 2:
+        pass
+    elif i == 3:
+        for j in range(10):
+            print("...")
+            if j < 5:
+                print("!!!!!")
+            elif j > 5:
+                print("?????????")
+            else:
+                while True:
+                    if j == 2:
+                        if j == 0:
+                            if j == 8:
+                                if j == 7:
+                                    print("doing very cool things forever and ever")
+elif i == 3:
+    if i == 2:
+        pass
+    elif i == 3:
+        for j in range(10):
+            print("...")
+            if j < 5:
+                print("!!!!!")
+            elif j > 5:
+                print("?????????")
+            else:
+                while True:
+                    print("doing very cool things forever and ever")
+    else:
+        if i == 2:
+            pass
+        elif i == 3:
+            for j in range(10):
+                print("...")
+                if j < 5:
+                    print("!!!!!")
+                elif j > 5:
+                    print("?????????")
+                else:
+                    pass
+else:
+    for i in range(10):
+        for j in range(10):
+            for k in range(10):
+                pass
+
+if j == 2:
+    if j == 0:
+        if j == 8:
+            if j == 7:
+                if j == 2:
+                    if j == 0:
+                        if j == 8:
+                            if j == 7:
+                                print("doing very cool things forever and ever")
+if i == 2:
+    for j in range(10):
+        if j == 3:
+            print("3")
+elif i == 3:
+    for j in range(10):
+        if j == 3:
+            print("3")
+
+if i == 2:
+    for j in range(10):
+        if j == 3:
+            print("3")
